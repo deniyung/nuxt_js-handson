98ac216e4fb5cb8cb1cec33b69c82577b1925d09
deniyung <deniyung@gmail.com>
2018-11-17 16:48:28 +0900

    first commit　

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <sample-component />
+</template>
+​
+<script>
+import SampleComponent from '~/components/SampleComponent.vue'
+
+export default {
+  components: {
+    SampleComponent
+  }
+}
+</script>
